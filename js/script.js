0881c291ebca939b6a5a9677ced2374389c34e5e
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-20 13:20:13 +0100

    finished header script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const headerInner = document.querySelector('#site-header > .header-inner');
-const logo = `<img src="http://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`
+const logo = `<img src="https://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`
+
+const pageLocation = document.title.toLowerCase();
+const pagesToShowLogo = [
+    'prijzen - taxi anker',
+    'contacteer taxi anker - taxi anker',
+    'algemene voorwaarden - taxi anker',
+    'privacybeleid - taxi anker'
+];
 
 function addLogo(e) {
     headerInner.insertAdjacentHTML('afterbegin', logo);
-}
+};
 
-addLogo();
+if (pagesToShowLogo.includes(pageLocation)) {
+    addLogo()
+};
