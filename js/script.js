152e2df0bf505ae10e10392d5ca8af0dfe59394c
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-20 13:24:12 +0100

    finished script

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-const headerInner = document.querySelector('#site-header > .header-inner');
-const logo = `<img src="https://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`
+document.addEventListener("DOMContentLoaded", function(e) { 
+    const headerInner = document.querySelector('#site-header > .header-inner');
+    const logo = `<img src="https://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`
 
-const pageLocation = document.title.toLowerCase();
-const pagesToShowLogo = [
-    'prijzen - taxi anker',
-    'contacteer taxi anker - taxi anker',
-    'algemene voorwaarden - taxi anker',
-    'privacybeleid - taxi anker'
-];
+    function addLogo(e) {
+        headerInner.insertAdjacentHTML('afterbegin', logo);
+    };
 
-function addLogo(e) {
-    headerInner.insertAdjacentHTML('afterbegin', logo);
-};
+    const pagesToShowLogo = [
+        'prijzen - taxi anker',
+        'contacteer taxi anker - taxi anker',
+        'algemene voorwaarden - taxi anker',
+        'privacybeleid - taxi anker'
+    ];
 
-if (pagesToShowLogo.includes(pageLocation)) {
-    addLogo()
-};
+    const pageLocation = document.title.toLowerCase();
+
+    if (pagesToShowLogo.includes(pageLocation)) {
+        addLogo()
+    };
+});
