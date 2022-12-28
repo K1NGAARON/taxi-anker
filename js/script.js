95f27db4da830695a06e1924416fdd20a0e7f6d1
K1NGAARON <gerardaaron1997@gmail.com>
2022-12-28 15:57:47 +0100

    fixed phone header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
     const headerInner = document.querySelector('#site-header > .header-inner');
-    const logo = `<img src="https://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`
-
+    
     function addLogo(e) {
+        const logo = `<img src="https://taxianker.be/wp-content/uploads/2022/11/logo.png" alt="Taxi Anker Logo">`;
         headerInner.insertAdjacentHTML('afterbegin', logo);
     };
 
     function removeOldHeader(e) {
         document.querySelector('.page-header').classList.add('hidden');
-    }
+    };
+
+    const addPhoneHome = () => {
+        const phoneNumber = `<a class="hide-lg" style="font-size: 20px; font-weight: 700;" href="tel:32492887798">+32 492 88 77 98</a>`;
+        headerInner.insertAdjacentHTML('beforeend', phoneNumber);
+    };
 
     const pagesToShowLogo = [
         'onze prijzen bij taxi anker',
@@ -23,4 +28,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
         addLogo();
         removeOldHeader();
     };
+
+    addPhoneHome();
 });
